56ef593d83fb579be7c20c5620ca363b8287061b
David Gillies <david.gillies.rhu@gmail.com>
2015-09-29 15:54:33 +0100

    latest templates

--- fs_proj/fs_renderer/plugins.py
+++ fs_proj/fs_renderer/plugins.py
@@ -2,6 +2,7 @@
 Plugins
 """
 import fs_apps
+from questionnaire.models import *
 
 class ChildList():
     def __init__(self, data, caller):
@@ -11,9 +12,6 @@ class ChildList():
 
     def get_rows(self):
         no_rows = int(self.data['FH02_SisterTotal']) + int(self.data['FH03_BrotherTotal'])
-        #for k, v in self.data:
-        #    if 'Sibling' in k and int(v) != -1:
-        #        print k
         rows = []
 
         for number in range(1, no_rows+1):
@@ -29,7 +27,6 @@ class ChildList():
                 child_age_value = ''
             child_data = {'child_name': child_name_value, 'child_age': child_age_value}
             rows.append(child_data)
-
         return rows
 
 
@@ -42,16 +39,22 @@ class TestPlugin():
         else:
             self.template = 'fs_renderer/testplugin.html'
 
-    def profile(self):
-        profile = {}
+    def do_it(self):
+
         if isinstance(self.caller,fs_apps.Question):
-            profile['name'] = self.caller.variable
-            profile['type'] = 'Question'
-            profile['position'] = self.caller.position
-            profile['section'] = self.caller.section.title
+            results = Results.objects.filter(var_name__startswith='child_age')
+            tally = 0
+            number = 0
+            for result in results:
+                if result.var_value != '':
+                    tally = tally + int(result.var_value)
+                    number = number + 1
+            average = tally/number        
+            return average
         if isinstance(self.caller,fs_apps.QuestionGroup):
+            profile = {}
             profile['name'] = self.caller.title
             profile['type'] = 'QuestionGroup'
             profile['position'] = self.caller.position
             profile['section'] = self.caller.section.title
-        return profile
+            return profile
